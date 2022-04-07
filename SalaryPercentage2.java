7094928399892031e3319746f08938100ef8d716
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2022-04-07 13:24:06 -0300

    Update SalaryPercentage2.java

--- SalaryPercentage2.java
+++ SalaryPercentage2.java
@@ -4,17 +4,17 @@ public class SalaryPercentage2 {
     public static void main(String[] args) {
 
         double salary = 5000;
-        double firstpercentage = 30;
-        double secondpercentage = 15;
-        double thirdpercentage = 5;
+        double firstPercentage = 30;
+        double secondPercentage = 15;
+        double thirdPercentage = 5;
 
-        double resultepercentage = salary * firstpercentage / 100;
+        double resultepercentage = salary * firstPercentage / 100;
         System.out.println("30% = " + resultepercentage);
 
-        resultepercentage = salary * secondpercentage /100;
+        resultepercentage = salary * secondPercentage /100;
         System.out.println("15% = " + resultepercentage);
 
-        resultepercentage = salary * thirdpercentage /100;
+        resultepercentage = salary * thirdPercentage /100;
         System.out.println("5% = " + resultepercentage);
 
     }
